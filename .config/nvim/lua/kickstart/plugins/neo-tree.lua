97923f883689d12481c0dd339c2591377a8a87d5
Muhammad Erwin Arif <muherwinarif31@gmail.com>
2024-08-24 13:16:08 +0800

    neo-treed config

--- .config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ .config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -15,10 +15,32 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true, -- Ensures all items are displayed in the tree
+        hide_dotfiles = false, -- Dotfiles are shown
+        hide_gitignored = false, -- Git-ignored files are shown
+        hide_hidden = false, -- Hidden files are shown
+        never_show = {}, -- Keep this empty to display all items
+        hide_by_name = { -- List of directories to ignore during filtering/searching
+          'node_modules',
+          '.git',
+          'vendor'
+          -- Add more directories as needed
+        },
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
         },
+      },
+    },
+    event_handlers = {
+      {
+        event = 'file_open_requested',
+        handler = function()
+          -- auto close Neo-tree when a file is opened
+          require('neo-tree.command').execute { action = 'close' }
+        end,
       },
     },
   },
