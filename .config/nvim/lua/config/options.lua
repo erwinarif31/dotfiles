4449bd1639b78fc7f812ab2fb95905bb455c777b
Muhammad Erwin Arif <muherwinarif31@gmail.com>
2025-08-09 21:19:47 +0800

    feat: add manual formatting for git hunks
    This commit introduces a new keymap, <leader>cF, to format only the changed hunks in a file, based on gitsigns. This provides more granular control over formatting and
    avoids autoformatting the entire file.
    
    The following changes are included:
    
    - A new keymap <leader>cF is added to trigger hunk formatting.
    - Autoformatting is disabled to prevent conflicts with the new manual approach.
    - Go formatters are configured for conform.nvim.

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,5 +3,6 @@
 -- Add any additional options here
 vim.opt.shiftwidth = 4
 vim.g.lazyvim_php_lsp = "intelephense"
+vim.g.autoformat = false
 
 vim.opt.swapfile = false
--- .config/nvim/lua/plugins/keymaps.lua
+++ .config/nvim/lua/plugins/keymaps.lua
@@ -1,6 +1,14 @@
 return {
-  "folke/snacks.nvim",
-  keys = {
-    { "<leader>sR", false },
+  {
+    "folke/snacks.nvim",
+    keys = {
+      { "<leader>sR", false },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    keys = {
+      { "<leader>cF", false },
+    },
   },
 }
